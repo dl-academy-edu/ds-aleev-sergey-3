f3f3ffd4c6ae0b18d0a95b1b9ff4135ebf219515
SergeyAleev <82897736+SergeyAleev@users.noreply.github.com>
2021-09-17 15:20:44 +0300

    Добавил вёрстки
    Добавил шрифты, эффекты, модальные окна и начал делать секцию с фильтром 0 своими чекбоксами и радио кнопками

--- src/pages/main/scripts.js
+++ src/pages/main/scripts.js
@@ -1,19 +1,34 @@
 let buttonSignIn = document.querySelector('.button-js-signIn')
-let modalSignIn = document.querySelector('.modal-popup')
+let buttonRegister = document.querySelector('.button-js-register')
+let buttonSendMessage = document.querySelector('.button-js-send-message')
+let modalOpen = document.querySelector('.modal-popup')
+let modalSignIn = document.querySelector('.modal-sign-in')
+let modalRegister = document.querySelector('.modal-register')
+let modalSendMessage = document.querySelector('.modal-send-message')
 let buttonClose = document.querySelector('.modal-close')
+let modalClose = document.querySelector('.modal-background')
 let input = modalSignIn.querySelector('input')
 
 buttonSignIn.addEventListener('click', function() {
+ modalClose.classList.add('modal-open')
  modalSignIn.classList.add('modal-open')
 })
-
+buttonRegister.addEventListener('click', function() {
+    modalClose.classList.add('modal-open')
+    modalRegister.classList.add('modal-open')
+})
+buttonSendMessage.addEventListener('click', function() {
+    modalClose.classList.add('modal-open')
+    modalSendMessage.classList.add('modal-open')
+})
 buttonClose.addEventListener('click', function() {
+    modalClose.classList.remove('modal-open')
+    modalRegister.classList.remove('modal-open')
     modalSignIn.classList.remove('modal-open')
 })
 
+// let buttonSubmit = document.querySelectorAll('.modal-content__button');
 
-let buttonSubmit = document.querySelectorAll('.modal-content__button')
-
-buttonSubmit.addEventListener('click', function(e) {
-    e.preventDefault()
-})
+// buttonSubmit.addEventListener('click', function(e) {
+//     e.preventDefault()
+// })
